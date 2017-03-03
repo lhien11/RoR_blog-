db6333e8ab73b2a522771eb9f60ae2402f5d95d7
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-03-03 03:20:40 -0600

    Added image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303023427) do
+ActiveRecord::Schema.define(version: 20170303081013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,9 @@ ActiveRecord::Schema.define(version: 20170303023427) do
     t.text     "body"
     t.text     "description"
     t.string   "slug"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "banner_image_url"
     t.index ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
   end
 
