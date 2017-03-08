a8851b52a89282831dfcb4e044c86296d005df40
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-03-07 23:48:13 -0600

    Everything works again ... fix authors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
 
-  get 'about' => 'pages#about', as: :about
-  get 'contact' => 'pages#contact', as: :contact
+  devise_for :authors
+  root to: 'blog/posts#index'
 
-  root to: 'posts#index'
-  resources :posts
+  namespace :authors do
+    resources :posts do
+    end
+  end
+
+  scope module: 'blog' do
+    get 'about' => 'pages#about', as: :about
+    get 'contact' => 'pages#contact', as: :contact
+    get 'posts' => 'posts#index', as: :posts
+    get 'posts/:id' => 'posts#show', as: :post
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
